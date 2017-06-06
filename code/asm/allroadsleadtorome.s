558f8bfa5733d7bb23e0ae9e9e3726d4d4cf132e
CPL0 <CPL0@protonmail.com>
2017-06-06 15:03:59 +0200

    commented allroadsleadtorome.s

--- code/asm/allroadsleadtorome.s
+++ code/asm/allroadsleadtorome.s
@@ -1,16 +1,16 @@
 global _start:
 section .text
-
+; assemble me, assemble me not... assemble me... assemble me not.. assemble me...
 start:
-	lea rax, [0]
-	shr rax, 32
-	shl rax, 32
-	movzx rax, byte [$+8]
-	movzx ax, byte [$+8]
-	sub rax, rax
-	and rax, 0
-	andn rax, rax, rax
-	imul rax, rax,0
-	xor rax, rax
-	bextr rax, rax, rax
-	sbb rax, rax
+	lea rax, [0]			; rax = 0
+	shr rax, 32                    	; rax = 0
+	shl rax, 32                    	; rax = 0
+	movzx rax, byte [$+8]           ; rax = 0
+	movzx ax, byte [$+8]            ; rax = 0
+	sub rax, rax                    ; rax = 0
+	and rax, 0                    	; rax = 0
+	andn rax, rax, rax              ; rax = 0
+	imul rax, rax,0                 ; rax = 0
+	xor rax, rax                    ; rax = 0
+	bextr rax, rax, rax             ; rax = 0
+	sbb rax, rax                    ; rax = 0
