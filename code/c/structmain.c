93208be6f058af7182cb97329677e071fb2a514a
voidfunk <voidfunk@protonmail.com>
2017-06-07 02:33:29 +0200

    edited c files

--- code/c/structmain.c
+++ code/c/structmain.c
@@ -1,7 +1,6 @@
 // variaton on code seen in weird main.
 // this time main is the declaration of a struct. :-)
-// compiles just fine. didnt test output on ubuntu/debian yet though.
-// does not work on my gentoo, output is not displayed. but runs just fine on ubuntu/debian default builds for me
+// compiles just fine.
 typedef struct {
 	int a[13];
 } _main;
