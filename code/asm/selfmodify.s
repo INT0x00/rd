d735ca56fb7200f2e91b7d3b6741d419632a00cc
voidfunk <voidfunk@protonmail.com>
2017-06-07 02:48:06 +0200

    fixed asm files

--- code/asm/selfmodify.s
+++ code/asm/selfmodify.s
@@ -1,19 +1,15 @@
 ; test file which modifies itself and then runs it's modified code.
 
-
 global _start
 section .text
 
 _start:
 
-	call make_exit  ; encode exit syscall into nopsled.
-	call aexit	; test our routine. (exit(0))
-
-
+	call aexit ; wait...
+	call aexit
 
 section .data
-
-aexit:			; fake exit routine.
+aexit:
 a:	db 0x90
 b:	db 0x90
 c:	db 0x90
@@ -26,13 +22,6 @@ i:	db 0x90
 j:	db 0x90
 k:	db 0x90
 l:	db 0x90
-
-	jmp $			; if we have nops here still when we call it
-				; then we will just hang cpu in running program.
-make_exit:	; we could fill it in differently, but we just move the litteral bytes
-		; for this example.
-		; the nop sled could be a set of xored or otherwise encoded bytes. aslong as it is
-		; an equal amount of bytes we wont have to fix any call or jmp offsets in the binary...
 	mov byte [a], 0xb8
 	mov byte [b], 0x3c
 	mov byte [c], 0x00
@@ -46,3 +35,4 @@ make_exit:	; we could fill it in differently, but we just move the litteral byte
 	mov byte [k], 0x0f
 	mov byte [l], 0x05
 	ret
+	; what?
